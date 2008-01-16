6fecbdcba794580eb10a17bdabc247cf2cbc6fcc
pammyla.fox <pammyla.fox@23318029-ee29-0410-bd5f-913a56701d96>
2008-01-16 19:26:37 +0000

    Marker manager reverted info window changes

--- markermanager/release/src/markermanager.js
+++ markermanager/release/src/markermanager.js
@@ -1,5 +1,7 @@
-/* Copyright (c) 2007 Google Inc.
- * 
+/* 
+ * MarkerManager, v1.0
+ * Copyright (c) 2007 Google Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. 
  *
- * Version: 1.0
+ *
  * Author: Doug Ricket, others
  * 
  * Marker manager is an interface between the map and the user, designed
@@ -90,17 +92,12 @@ function MarkerManager(map, opt_opts) {
   // NOTE: These two closures provide easy access to the map.
   // They are used as callbacks, not as methods.
   me.removeOverlay_ = function(marker) {
-    if (!marker.isInfoWindowOpened) {
-      map.removeOverlay(marker);
-      me.shownMarkers_--;
-    }
+    map.removeOverlay(marker);
+    me.shownMarkers_--;
   };
   me.addOverlay_ = function(marker) {
-    if (!marker.isInfoWindowOpened) {
-      marker.isInfoWindowOpened = false;
-      map.addOverlay(marker);
-      me.shownMarkers_++;
-    }
+    map.addOverlay(marker);
+    me.shownMarkers_++;
   };
 
   me.resetManager_();
@@ -177,14 +174,6 @@ MarkerManager.prototype.addMarkerBatch_ = function(marker, minZoom, maxZoom) {
   if (this.trackMarkers_) {
     GEvent.bind(marker, "changed", this, this.onMarkerMoved_);
   }
-
-  GEvent.addListener(marker, "infowindowopen", function() {
-    marker.isInfoWindowOpened = true;
-  });
-
-  GEvent.addListener(marker, "infowindowclose", function() {
-    marker.isInfoWindowOpened = false;
-  });
 
   var gridPoint = this.getTilePoint_(mPoint, maxZoom, GSize.ZERO);
 
