b87206f3f7478865092df40da3d11a1c09dca6f2
pamela.fox <pamela.fox@23318029-ee29-0410-bd5f-913a56701d96>
2008-03-28 00:25:56 +0000

    Committing MapIconMaker in release

--- trunk/mapiconmaker/release/src/mapiconmaker.js
+++ trunk/mapiconmaker/release/src/mapiconmaker.js
@@ -1,5 +1,5 @@
 /**
- * MapIconFactory v1.0
+ * MapIconMaker v1.0
  * Copyright (c) 2008 Pamela Fox
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,9 +21,9 @@
  *  colored marker icons using the Charts API marker output.
  */
 
-var MapIconFactory = {};
+var MapIconMaker = {};
 
-MapIconFactory.createMarkerIcon = function(opts) {
+MapIconMaker.createMarkerIcon = function(opts) {
   var width = opts.width || 32;
   var height = opts.height || 32;
   var primaryColor = opts.primaryColor || "#ff0000";
