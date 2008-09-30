b93c8fb682f61cde7c842cc70d7232a64fbcd861
pamela.fox <pamela.fox@23318029-ee29-0410-bd5f-913a56701d96>
2008-09-30 07:45:56 +0000

    Patching in fix and style issues

--- mapiconmaker/1.1/src/mapiconmaker.js
+++ mapiconmaker/1.1/src/mapiconmaker.js
@@ -72,7 +72,7 @@ var MapIconMaker = {};
  * @param {MarkerIconOptions} [opts]
  * @return {GIcon}
  */
-MapIconMaker.createMarkerIcon = function(opts) {
+MapIconMaker.createMarkerIcon = function (opts) {
   var width = opts.width || 32;
   var height = opts.height || 32;
   var primaryColor = opts.primaryColor || "#ff0000";
@@ -81,30 +81,34 @@ MapIconMaker.createMarkerIcon = function(opts) {
    
   var baseUrl = "http://chart.apis.google.com/chart?cht=mm";
   var iconUrl = baseUrl + "&chs=" + width + "x" + height + 
-      "&chco=" + cornerColor.replace("#", "") + "," + primaryColor.replace("#", "") + "," + strokeColor.replace("#", "") + "&ext=.png";
+      "&chco=" + cornerColor.replace("#", "") + "," + 
+      primaryColor.replace("#", "") + "," + 
+      strokeColor.replace("#", "") + "&ext=.png";
   var icon = new GIcon(G_DEFAULT_ICON);
   icon.image = iconUrl;
   icon.iconSize = new GSize(width, height);
-  icon.shadowSize = new GSize(Math.floor(width*1.6), height);
-  icon.iconAnchor = new GPoint(width/2, height);
-  icon.infoWindowAnchor = new GPoint(width/2, Math.floor(height/12));
+  icon.shadowSize = new GSize(Math.floor(width * 1.6), height);
+  icon.iconAnchor = new GPoint(width / 2, height);
+  icon.infoWindowAnchor = new GPoint(width / 2, Math.floor(height / 12));
   icon.printImage = iconUrl + "&chof=gif";
   icon.mozPrintImage = iconUrl + "&chf=bg,s,ECECD8" + "&chof=gif";
   iconUrl = baseUrl + "&chs=" + width + "x" + height + 
-      "&chco=" + cornerColor.replace("#", "") + "," + primaryColor.replace("#", "") + "," + strokeColor.replace("#", "");
+      "&chco=" + cornerColor.replace("#", "") + "," + 
+      primaryColor.replace("#", "") + "," + 
+      strokeColor.replace("#", "");
   icon.transparent = iconUrl + "&chf=a,s,ffffff11&ext=.png";
 
   icon.imageMap = [
-      width/2, height,
-      (7/16)*width, (5/8)*height,
-      (5/16)*width, (7/16)*height,
-      (7/32)*width, (5/16)*height,
-      (5/16)*width, (1/8)*height,
-      (1/2)*width, 0,
-      (11/16)*width, (1/8)*height,
-      (25/32)*width, (5/16)*height,
-      (11/16)*width, (7/16)*height,
-      (9/16)*width, (5/8)*height
+    width / 2, height,
+    (7 / 16) * width, (5 / 8) * height,
+    (5 / 16) * width, (7 / 16) * height,
+    (7 / 32) * width, (5 / 16) * height,
+    (5 / 16) * width, (1 / 8) * height,
+    (1 / 2) * width, 0,
+    (11 / 16) * width, (1 / 8) * height,
+    (25 / 32) * width, (5 / 16) * height,
+    (11 / 16) * width, (7 / 16) * height,
+    (9 / 16) * width, (5 / 8) * height
   ];
   for (var i = 0; i < icon.imageMap.length; i++) {
     icon.imageMap[i] = parseInt(icon.imageMap[i]);
@@ -122,7 +126,7 @@ MapIconMaker.createMarkerIcon = function(opts) {
  * @param {MarkerIconOptions} [opts]
  * @return {GIcon}
  */
-MapIconMaker.createFlatIcon = function(opts) {
+MapIconMaker.createFlatIcon = function (opts) {
   var width = opts.width || 32;
   var height = opts.height || 32;
   var primaryColor = opts.primaryColor || "#ff0000";
@@ -131,30 +135,32 @@ MapIconMaker.createFlatIcon = function(opts) {
   var labelColor = opts.labelColor || "#000000";
   var labelSize = opts.labelSize || 0;
   var shape = opts.shape ||  "circle";
-  var shapeCode = (shape == "circle") ? "it" : "itr";
+  var shapeCode = (shape === "circle") ? "it" : "itr";
 
   var baseUrl = "http://chart.apis.google.com/chart?cht=" + shapeCode;
   var iconUrl = baseUrl + "&chs=" + width + "x" + height + 
-      "&chco=" + primaryColor.replace("#", "") + "," + shadowColor.replace("#", "") + "ff,ffffff01" +
-      "&chl=" + label + "&chx=" + labelColor.replace("#", "") + "," + labelSize;
+      "&chco=" + primaryColor.replace("#", "") + "," + 
+      shadowColor.replace("#", "") + "ff,ffffff01" +
+      "&chl=" + label + "&chx=" + labelColor.replace("#", "") + 
+      "," + labelSize;
   var icon = new GIcon(G_DEFAULT_ICON);
   icon.image = iconUrl + "&chf=bg,s,00000000" + "&ext=.png";
   icon.iconSize = new GSize(width, height);
   icon.shadowSize = new GSize(0, 0);
-  icon.iconAnchor = new GPoint(width/2, height/2);
-  icon.infoWindowAnchor = new GPoint(width/2, height/2);
+  icon.iconAnchor = new GPoint(width / 2, height / 2);
+  icon.infoWindowAnchor = new GPoint(width / 2, height / 2);
   icon.printImage = iconUrl + "&chof=gif";
   icon.mozPrintImage = iconUrl + "&chf=bg,s,ECECD8" + "&chof=gif";
   icon.transparent = iconUrl + "&chf=a,s,ffffff01&ext=.png";
   icon.imageMap = []; 
-  if (shapeCode == "roundrect") {
+  if (shapeCode === "itr") {
     icon.imageMap = [0, 0, width, 0, width, height, 0, height];
   } else {
     var polyNumSides = 8;
-    var polySideLength = 360/polyNumSides;
-    var polyRadius = Math.min(width, height)/2;
-    for (var a = 0; a<(polyNumSides+1); a++) {
-      var aRad = polySideLength*a*(Math.PI/180);
+    var polySideLength = 360 / polyNumSides;
+    var polyRadius = Math.min(width, height) / 2;
+    for (var a = 0; a < (polyNumSides + 1); a++) {
+      var aRad = polySideLength * a * (Math.PI / 180);
       var pixelX = polyRadius + polyRadius * Math.cos(aRad);
       var pixelY = polyRadius + polyRadius * Math.sin(aRad);
       icon.imageMap.push(parseInt(pixelX), parseInt(pixelY));
@@ -173,7 +179,7 @@ MapIconMaker.createFlatIcon = function(opts) {
  * @param {MarkerIconOptions} [opts]
  * @return {GIcon}
  */
-MapIconMaker.createLabeledMarkerIcon = function(opts) {
+MapIconMaker.createLabeledMarkerIcon = function (opts) {
   var primaryColor = opts.primaryColor || "#DA7187";
   var strokeColor = opts.strokeColor || "#000000";
   var starPrimaryColor = opts.starPrimaryColor || "#FFFF00";
@@ -184,12 +190,14 @@ MapIconMaker.createLabeledMarkerIcon = function(opts) {
   
   var pinProgram = (addStar) ? "pin_star" : "pin";
   var baseUrl = "http://chart.apis.google.com/chart?cht=d&chdp=mapsapi&chl=";
-  var iconUrl = baseUrl + pinProgram + "'i\\" + "'[" + label + "'-2'f\\"  + "hv'a\\]" + "h\\]o\\" + 
+  var iconUrl = baseUrl + pinProgram + "'i\\" + "'[" + label + 
+      "'-2'f\\"  + "hv'a\\]" + "h\\]o\\" + 
       primaryColor.replace("#", "")  + "'fC\\" + 
       labelColor.replace("#", "")  + "'tC\\" + 
       strokeColor.replace("#", "")  + "'eC\\";
   if (addStar) {
-    iconUrl += starPrimaryColor.replace("#", "") + "'1C\\" + starStrokeColor.replace("#", "") + "'0C\\";
+    iconUrl += starPrimaryColor.replace("#", "") + "'1C\\" + 
+        starStrokeColor.replace("#", "") + "'0C\\";
   }
   iconUrl += "Lauto'f\\";
 
@@ -205,7 +213,7 @@ MapIconMaker.createLabeledMarkerIcon = function(opts) {
  *  and then typical URL escaping. Must be applied to user-supplied text.
  * @private
  */
-MapIconMaker.escapeUserText_ = function(text) {
+MapIconMaker.escapeUserText_ = function (text) {
   if (text === undefined) {
     return null;
   }
